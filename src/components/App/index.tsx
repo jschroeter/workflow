fdd3cc464729c47d75ea2ccf40b86d1508a7f020
Jakob Schröter <jakob@schroetermedien.de>
2018-03-16 02:05:03 +0100

    first very basic data sync via websockets

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -2,8 +2,12 @@ import * as React from 'react';
 import './style.css';
 import Diagram from '../Diagram';
 import Bar from '../Bar';
+import * as io from 'socket.io-client';
+import { DiagramModel } from 'storm-react-diagrams';
 
 class App extends React.Component {
+  socket = null;
+
   state = {
     engine: null
   };
@@ -15,6 +19,35 @@ class App extends React.Component {
   setEngine = engine => {
     this.setState({ engine });
   };
+
+  componentDidMount() {
+    this.socket = io('http://localhost:3001').on('datachange', data => {
+      this.setDiagramModel(data);
+    });
+  }
+
+  setDiagramModel(data) {
+    const newModel = new DiagramModel();
+    newModel.deSerializeDiagram(data, this.state.engine);
+
+    newModel.addListener({
+      nodesUpdated: () => {
+        if (this.socket) {
+          const newData = newModel.serializeDiagram();
+          this.socket.emit('datachange', newData);
+        }
+      },
+      linksUpdated: () => {
+        if (this.socket) {
+          const newData = newModel.serializeDiagram();
+          this.socket.emit('datachange', newData);
+        }
+      }
+    });
+
+    this.state.engine.setDiagramModel(newModel);
+    this.state.engine.repaintCanvas();
+  }
 
   render() {
     return (
